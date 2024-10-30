bccafa9fc35185f6bdd0af032f3db562e93b9afd
Synpath <lengjianing@gmail.com>
2024-10-29 21:50:56 -0400

    Added Skip text writing functionality
    help why is my computer tweaking right now

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
 let gameStart = false;
 let writingText = false;
+let gameEnd = false;
+let isSkipping = false;
 
 //start the game
 document.addEventListener('keyup', function(event) {
   if (((event.key == 'Enter') && (!gameStart)) && (!writingText)) {
       gameStart = true;
       startGame();
-    
   }
-  
 });
 
 //print out commands list
@@ -19,36 +19,63 @@ document.addEventListener('keyup', function(event) {
   }
 });
 
+// skip text writing
+document.addEventListener('keyup', function(event) {
+  if ((event.key == " ") && (writingText)) {
+      isSkipping = true;
+  }
+});
+
 function startGame() {
   document.getElementById("content").innerHTML += '<hr>'; //add a divider after game start
-  typeWriter("startText", "content", 50); //write the starting text of the game
   document.getElementById("content").innerHTML += document.getElementById("commands").innerHTML //print out the commands list for the first time
+  typeWriter("startText", "content", 50);
+    writeText('\n', "content");
+    skipText(0, "chooseGrave", "content");
+    console.log("Hello");
+  
+}
 
+function writeText(char, target) {
+  if (char === '\n') {
+    document.getElementById(target).innerHTML += '<br>'; // Add a line break
+  } else {
+    document.getElementById(target).innerHTML += char;
+  }
+}
 
-
+//
+function skipText(num, content, target) {
+  text = document.getElementById(content).textContent;
+  for (num; num < text.length; num++) {
+    const char = text.charAt(num);
+    writeText(char, target);
+  }
 }
 
 // Typewriter function
 // "text": id of the text block
 // "targetElement": html paragraph that will be changed 
-function typeWriter(text, targetElement, speed) {
+async function typeWriter(text, targetElement, speed) {
     textCon = document.getElementById(text).textContent;  
-    //document.getElementById(targetElement).innerHTML += textCon;
     writingText = true;
+    
     let i = 0;
     let timer = setInterval(() => {
-      if (i < textCon.length) {
+      if ((i < textCon.length) && (!isSkipping)) {
         const char = textCon.charAt(i);
-        if (char === '\n') {
-          document.getElementById(targetElement).innerHTML += '<br>'; // Add a line break
-        } else {
-          document.getElementById(targetElement).innerHTML += char;
-        }
+        writeText(char, targetElement);
         i++;
+      } else if (isSkipping) {
+        clearInterval(timer);
+        skipText(i, text, targetElement);
+        writingText = false;
+        isSkipping = false;
       } else {
         clearInterval(timer);
         writingText = false;
       }
     }, speed);
+
 }
   
